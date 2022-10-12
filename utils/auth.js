044a310628872f8498d3bd25719d5627248c7a14
colinplancich <colin.plancich@gmail.com>
2022-10-11 17:37:12 -0700

    added some files and todo's

---
+++ utils/auth.js
@@ -0,0 +1,18 @@
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  
+  const withAuthApi = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.status(403).json({ message: "you are not authorized to do that"})
+    } else {
+      next();
+    }
+  };
+  module.exports = {withAuth, withAuthApi};
+  
